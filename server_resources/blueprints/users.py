d198788c94039a61537ee9653314265269dcdd2d
Petr Liakhavets <petr_liakhavets@epam.com>
2020-05-14 06:07:05 +0300

    added 'add' method to users

--- server_resources/blueprints/users.py
+++ server_resources/blueprints/users.py
@@ -13,11 +13,11 @@ from server_resources.blueprints.auth import login_required
 blueprint = Blueprint('users', __name__, url_prefix='/users')
 
 
-@blueprint.route('/list_users')
+@blueprint.route('/list_users', methods=('GET', 'POST'))
 @login_required
 def list_users():
     if g.user.admin:
-        return render_template("list/user_list.html", users=User.query.all())
+        return render_template("list/user_list.html", user_list=User.query.all())
     else:
         flash("Unauthorized access", "warning")
         return redirect(url_for("index"))
@@ -59,3 +59,21 @@ def update(name):
         user.password = new_password
     db.session.commit()
     return redirect(url_for('users.show', name=name))
+
+
+@blueprint.route("/add", methods=["POST"])
+def add():
+    email = request.form["email"]
+    name = request.form['name']
+    password = request.form["password"]
+    warning = None
+    if db.session.query(User.query.filter_by(name=name).exists()).scalar():
+        warning = f"User {name} is already registered."
+    elif db.session.query(User.query.filter_by(email=email).exists()).scalar():
+        warning = "This email is already registered."
+    if warning is None:
+        db.session.add(User(name=name, email=email, password=password))
+        db.session.commit()
+        return redirect(url_for("users.list_users"))
+    flash(warning, 'warning')
+    return redirect(url_for("users.list_users"))
